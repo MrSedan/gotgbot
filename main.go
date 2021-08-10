12502a6ade371a2383c1acb5fa91342c04189c35
MrSedan <elpashevs@bk.ru>
2021-08-10 23:16:20 +0300

    Minimal bot

--- main.go
+++ main.go
@@ -1,7 +1,45 @@
 package main
 
-import "fmt"
+import (
+	"log"
+	"os"
+
+	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api"
+	"github.com/joho/godotenv"
+)
+
+func init() {
+	// Загрузка ключа из .env файла
+	if err := godotenv.Load(); err != nil {
+		log.Print("No env file")
+	}
+
+}
 
 func main() {
-	fmt.Println("Test")
+	key, exists := os.LookupEnv("API_KEY")
+	if !exists {
+		log.Panic("No api key!")
+	}
+	bot, err := tgbotapi.NewBotAPI(key)
+	if err != nil {
+		log.Panic(err)
+	}
+	bot.Debug = true
+	log.Printf("Authorized on account %s", bot.Self.UserName)
+	var ucfg tgbotapi.UpdateConfig = tgbotapi.NewUpdate(0)
+	ucfg.Timeout = 60
+	updates, err := bot.GetUpdatesChan(ucfg)
+	for update := range updates {
+		if update.Message == nil { // ignore any non-Message Updates
+			continue
+		}
+
+		log.Printf("[%s] %s", update.Message.From.UserName, update.Message.Text)
+
+		msg := tgbotapi.NewMessage(update.Message.Chat.ID, update.Message.Text)
+		msg.ReplyToMessageID = update.Message.MessageID
+
+		bot.Send(msg)
+	}
 }
